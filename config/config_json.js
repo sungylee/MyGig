696d3752ad002e9769d9b4bdf6d80eb07f1fce29
ronald chaudry <rchaud03@gmail.com>
2018-10-18 18:46:09 -0400

    revices schema 2

---
+++ config/config_json.js
@@ -0,0 +1,26 @@
+{
+  "development": {
+    "username": "root",
+    "password": "bootcamp",
+    "database": "gigazon",
+    "host": "127.0.0.1",
+    "port": 8889,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": bootcamp,
+    "database": "gigazon",
+    "host": "127.0.0.1",
+    "port": 8889,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "bootcamp",
+    "database": "gigazon",
+    "host": "127.0.0.1",
+    "port": 8889,
+    "dialect": "mysql"
+  }
+}
