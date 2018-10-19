b91ac76965ef9cec4554e1bda00e075c59eb8f9c
Jay Cha <kimchikimchi@gmail.com>
2018-10-19 18:49:32 -0400

    - In seeds.sql:   -- Add createdAt and updatedAt columns as these are created automatically by sequelize and can't be omitted for seeding.   -- Drop password field now as sequelize doens't have it. - In  projects.js   -- Futher fix of syntax. - In user.js   -- Fixed typo for 'currentPosition'   -- Allow phone to be null

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,15 @@
+use gigazon;
+
+insert into projects (name, description, product, projectStartDate, projectDuration, skills, createdAt, updatedAt) values ("testProj", "Just testing","the product", "2018-12-19", 2, "java", now(), now());
+
+
+insert into users (employeeID, firstName, lastName, currentPosition, managerID,  email,  createdAt, updatedAt) values (212000000, "jay", "cha", "Sr. Site Reliability Engineer", "2121110000", "jay,cha@ge.com", now(), now());
+insert into users (employeeID, firstName, lastName, currentPosition, managerID,  email,  createdAt, updatedAt) values (212000001, "ronald", "chaudry", "Site Reliability Engineer", "2121110000", "ronald.chaudry@ge.com", now(), now());
+insert into users (employeeID, firstName, lastName, currentPosition, managerID, email, createdAt, updatedAt) values (212000003, "siraj", "mohamed", "Sr. Network Operations Engineer", "2121110002", "siraj.mohamed@ge.com", now(), now());
+insert into users (employeeID, firstName, lastName, currentPosition, managerID, email, createdAt, updatedAt) values (212000004, "john", "steskal", "Sr. Site Reliability Engineer", "2121110003", "john.steskal@ge.com", now(), now());
+insert into users (employeeID, firstName, lastName, currentPosition, managerID, email, createdAt, updatedAt) values (212000005,"sung", "lee", "Sr. Site Reliability Engineer", "2121110001","sung.lee@ge.com", now(), now());
+
+/*  THE FOLLOWING WORKS FOR TABLES CREATED BY NON-SEQUELIZE ONLY
 insert into projects (name, description, product, projectStartDate, projectDuration,skills) values ("testProj", "Just testing","the product", "2018-12-19", 2,"java");
 
 insert into users (employeeID, password,  firstName, lastName, currentPosition, managerID,  email) values (212000000, "password124", "jay", "cha", "Sr. Site Reliability Engineer", "2121110000", "jay,cha@ge.com");
@@ -5,3 +17,4 @@ insert into users (employeeID, password,  firstName, lastName, currentPosition, 
 insert into users (employeeID, password,  firstName, lastName, currentPosition, managerID, email) values (212000003, "password126", "siraj", "mohamed", "Sr. Network Operations Engineer", "2121110002", "siraj.mohamed@ge.com");
 insert into users (employeeID, password,  firstName, lastName, currentPosition, managerID, email) values (212000004, "password127", "john", "steskal", "Sr. Site Reliability Engineer", "2121110003", "john.steskal@ge.com");
 insert into users (employeeID, password,  firstName, lastName, currentPosition, managerID, email) values (212000005,"password128",  "sung", "lee", "Sr. Site Reliability Engineer", "2121110001","sung.lee@ge.com");
+*/
