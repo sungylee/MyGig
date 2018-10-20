a20e5211e12869872843126934aa7cc5f42b9202
SteskalJ <john.steskal@ge.com>
2018-10-20 16:11:02 -0400

    Fixed user model

--- models/user.js
+++ models/user.js
@@ -10,19 +10,22 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	lastName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	currentPostion: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1,100]
-      },
+      }
+    },
 	managerID: {
 		type: DataTypes.INTEGER,
 		allowNull: false
@@ -32,7 +35,8 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	phone: {
 		type: DataTypes.INTEGER,
 		allowNull: false
@@ -41,57 +45,62 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
-	skill1: {
-      type: DataTypes.STRING,
-      validate: {
-        len: [1,50]
-      },
+      }
+    },
 	skill2: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill3: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill4: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill5: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill6: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill7: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill8: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill9: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
-      },
+      }
+    },
 	skill10: {
       type: DataTypes.STRING,
       validate: {
         len: [1,50]
       }
+    }
   });
 
   User.associate = function(models) {
