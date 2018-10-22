3f36b27abf6bf357a613b35ab40d7c27c8d65107
Jay Cha <kimchikimchi@gmail.com>
2018-10-22 16:22:11 -0400

    - Allow config/config.json to be checked in as this is getting really   annoying. - Incorporated John's email module into controllers. - For new application, upon adding a new application db entry, make http   call to localhost.../api/notify/<role>/<application id>.   Might be useful to shard the backend code - Adding helper function to cut down redundnacy in ApplicationController

--- controllers/apiApplicationsController.js
+++ controllers/apiApplicationsController.js
@@ -1,4 +1,5 @@
 var router = require("express").Router();
+var request = require("request");
 var db = require("../models");
 
 // GET all projects
@@ -16,8 +17,9 @@ router.get('/applications', function(req, res) {
 // POST a new project
 router.post('/applications', function(req, res) {
     console.log("Hitting post - /api/applications");
-    console.log(req.body);
+    //console.log(req.body);
     /*
+        When JSON input from http client is like this:
         {
             "ProjectProjectId" : 1,
             "UserEmployeeId": 212000000,
@@ -25,14 +27,27 @@ router.post('/applications', function(req, res) {
          }
     */
 
-    db.Application.create(req.body)
-    .then(function(application) {
+    db.Application.create(
+        req.body
+    ).then(function(application) {
         res.json(application);
-    })
-    .catch(function(error) {
+        return application;
+    }).then(application => {
+        send_email({
+            url: `http://localhost:3000/api/notify/applicant/${application.applicationId}`,
+            subject: "Your application has submitted",
+            body: `Your application for project id: ${req.body.ProjectProjectId} has been submitted.  Your application id is ${application.applicationId}.`
+        });
+
+        send_email({
+            url: `http://localhost:3000/api/notify/manager/${application.applicationId}`,
+            subject: "Your direct direct import has submitted application",
+            body: `Your direct report's application for project id: ${req.body.ProjectProjectId} has been submitted.  His/Her application id is ${application.applicationId}.`
+        });
+    }).catch(function(error) {
+        //TODO:  Should build a better query result check in case of failures.
         console.log(error);
     });
-    //TODO:  Should build a better query result check in case of failures.
 });
 
 // UPDATE a given project details
@@ -50,6 +65,25 @@ router.put('/applications/:applicationId', function(req, res) {
         // returns [ number_of_rows ]
         res.json(application);
     });
+
+    // Should put notification upon approval
 });
+
+function send_email(msg) {
+    request({
+        method: 'POST',
+        url: msg.url,
+        json: true,
+        body: {
+            subject: msg.subject,
+            msg: msg.body
+        }
+    }, function(err, res, body) {
+        if (err) {
+            console.error('error posting json: ', err);
+            throw err;
+        }
+    });
+}
 
 module.exports = router;
