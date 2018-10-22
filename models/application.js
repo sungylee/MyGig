2807d9958d85b82a19ba57e50920579a4c6eff8b
Jay Cha <kimchikimchi@gmail.com>
2018-10-21 20:38:28 -0400

    - Changed applicationID to applicationId to be consistent across all tables

--- models/application.js
+++ models/application.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
     var Application = sequelize.define("Application", {
-        applicationID: {
+        applicationId: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
